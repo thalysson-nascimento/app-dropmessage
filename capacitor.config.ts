91c95d7e3a0c90e70c64f9293c459a9ea35ded8b
thalysson-nascimento <thalyssonwg.nascimento@gmail.com>
2024-11-17 09:20:08 -0300

    feat: atualizado icone nome da aplicacao

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'br.com.dropmessage',
-  appName: 'ShotMessage',
+  appName: 'DatingMatch',
   webDir: 'dist/app-dropmessage/browser',
 };
 
