d8d0ea8b79d64342836fb24f8a06624288d863cb
thalysson-nascimento <thalyssonwg.nascimento@gmail.com>
2024-12-15 20:42:03 -0300

    feat: incluido input de codigo de verificacao de email

--- src/app/pages/home/verify-token-email/verify-token-email.component.ts
+++ src/app/pages/home/verify-token-email/verify-token-email.component.ts
@@ -1,12 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { LoadingComponent } from '../../../shared/component/loading/loading.component';
 import { ButtonStyleDirective } from '../../../shared/directives/button-style/button-style.directive';
+import { InputCustomDirective } from '../../../shared/directives/input-custom/input-custom.directive';
 
-const SharedComponents = [ButtonStyleDirective, LoadingComponent];
+const SharedComponents = [
+  ButtonStyleDirective,
+  LoadingComponent,
+  InputCustomDirective,
+];
 
-const CoreModule = [CommonModule];
+const CoreModule = [CommonModule, ReactiveFormsModule];
 
 @Component({
   selector: 'app-verify-token-email',
@@ -18,34 +28,36 @@ const CoreModule = [CommonModule];
 export class VerifyTokenEmailComponent implements OnInit {
   buttonDisalbled: boolean = false;
   isLoadingButton: boolean = false;
-  private route = inject(ActivatedRoute);
-  token!: string;
+  codeConfirmationEmailFormGroup!: FormGroup;
 
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
-      const token = params['token'];
-      if (token) {
-        this.verifyEmail(token);
-      }
+    this.codeConfirmationEmailFormBuilder();
+  }
+
+  codeConfirmationEmailFormBuilder() {
+    this.codeConfirmationEmailFormGroup = this.formBuilder.group({
+      codeConfirmationEmail: [
+        '',
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)],
+      ],
     });
   }
 
-  private verifyEmail(token: string) {
-    console.log(token);
-    this.token = token;
-    // this.authService.verifyEmail(token).subscribe({
-    //   next: (response) => {
-    //     // Tratar sucesso
-    //     console.log('Email verificado com sucesso');
-    //   },
-    //   error: (error) => {
-    //     // Tratar erro
-    //     console.error('Erro na verificação de email', error);
-    //   }
-    // });
+  onInputChange(event: any): void {
+    const input = event.target;
+    const maxLength = 6;
+
+    // Limita o número de caracteres
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength);
+    }
   }
 
-  sendEmailConfirmation() {}
+  sendCodeEmailConfirmation() {
+    this.isLoadingButton = true;
+    this.buttonDisalbled = true;
+    console.log('clicado');
+  }
 }
